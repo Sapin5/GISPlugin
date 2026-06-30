5b1cabd6045ca28d473a01aaea7927ecd70d3468
Sapin5 <123406040+Sapin5@users.noreply.github.com>
2026-06-30 11:18:21 -0500

    Just adding comments

--- Plugins/GIS_Terrain_Generator/Content/Python/SubProcessFile.py
+++ Plugins/GIS_Terrain_Generator/Content/Python/SubProcessFile.py
@@ -4,19 +4,28 @@ import os
 from pathlib import Path
 
 main_file_dir = os.path.join(Path(__file__).parent, "GIS_Data_Processor.py")
-engine_python = Path(os.environ.get("UE_PYTHONPATH", "")) 
-engine_python_2 = (Path(sys.prefix) / "python.exe")
+engine_python = (Path(sys.prefix) / "python.exe")
 
 """
     This file only exists because calling GIS_Data_Processor caused unreal to freeze up
     and creating a subprocess in c++ was more compilcated
 
-    May change in the future, I dont like having another 
+    TO DO:
+    Figure out a way to poll this
+    possibly just make:
+
+    def poll():
+        if process.poll() is None:
+            return false
+        return true
+    
+    I feel like this would work but the way unrel has been treating python makes me think otherwise
+    Since this would have to called from unreal c++, then theres the issue of if it hogs the main thread
+
 """
 
 def main(file_path):
-    subprocess.Popen([str(engine_python_2), main_file_dir, file_path],
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE,
+    process = subprocess.Popen([str(engine_python), main_file_dir, file_path],
                                 creationflags=8)
-
+    
+    return process
